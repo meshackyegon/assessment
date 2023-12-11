b2d99c0d3beae5e45bf3c06cf99931344904309c
Meshack Yegon <95590332+meshackyegon@users.noreply.github.com>
2023-12-11 19:15:29 +0300

    Update urls.py
    sow swagger in vercel app

--- trial/urls.py
+++ trial/urls.py
@@ -17,6 +17,7 @@ schema_view=get_schema_view(
 )
 
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('health/', include('health.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
